2125aceecc27ead334bb9f7f0ce69bf7c62f9a35
Ashot Nazaryan <ashotnazaryan45@outlook.com>
2021-06-04 12:57:36 +0300

    Implement initial logic

--- src/modules/Sudoku/Sudoku.styles.ts
+++ src/modules/Sudoku/Sudoku.styles.ts
@@ -5,37 +5,48 @@ export const Container = styled.div`
   padding: 10px;
 `;
 
-export const Difficulty = styled.div`
+export const Complexity = styled.div`
   padding: 10px 0;
 `;
 
 export const Board = styled.div.attrs(props => ({
-  size: props.id,
+  size: props.id
 }))`
+  width: ${props => Number(props.size) * 50}px;
   display: grid;
   grid-gap: 0;
-  grid-template-columns: repeat(${props => Number(props.size) / 3}, 150px);
-  grid-template-rows: repeat(${props => Number(props.size) / 3}, 150px);
+  grid-template-columns: repeat(${props => Number(props.size)}, 50px);
+  grid-template-rows: repeat(${props => Number(props.size)}, 50px);
   grid-auto-flow: row;
-
-  .sub-board {
-    display: grid;
-    grid-gap: 0;
-    grid-template-columns: repeat(${props => Number(props.size) / 3}, 50px);
-    grid-template-rows: repeat(${props => Number(props.size) / 3}, 50px);
-    grid-auto-flow: row;
-    box-shadow: 0 0 3px #666;
-  }
+  border: solid #666;
+  border-width: 2px 0px 0px 2px;
 
   .cell {
+    border: none;
+    border-color: none;
+    border-image: none;
     border-style: none;
     box-shadow: 0px 0px 3px #aaa;
     font-size: 35px;
     padding: 14px;
     box-sizing: border-box;
 
+    &::-webkit-outer-spin-button,
+    &::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+
+    &[type=number] {
+      -moz-appearance: textfield;
+    }
+
     &:focus-visible {
       outline: none;
+    }
+
+    &:disabled {
+      border: none;
     }
   }
 `;
--- src/modules/Sudoku/Sudoku.tsx
+++ src/modules/Sudoku/Sudoku.tsx
@@ -1,121 +1,302 @@
 import * as React from "react";
 
-import { Container, Difficulty, Board } from "./Sudoku.styles";
+import {
+  Container,
+  Complexity as ComplexityStyle,
+  Board,
+} from "./Sudoku.styles";
 
 interface SudokuState {
-  difficulty: string;
+  complexity: ComplexityTypes;
   board: Cell[][];
 }
 
 interface SudokuProps {}
 
+// TODO move to separate model file
 interface Cell {
   x: number;
   y: number;
   id: string;
   value: string;
+  sectionRowIndex: number;
+  sectionColumnIndex: number;
+  disabled?: boolean;
 }
 
-enum DifficultyTypes {
-  very_easy,
-  easy,
-  normal,
-  hard,
-  very_hard,
+interface Complexity {
+  key: string;
+  label: string;
 }
 
-const difficulties = [
-  { key: DifficultyTypes.very_easy, value: "Very easy" },
-  { key: DifficultyTypes.easy, value: "Easy" },
-  { key: DifficultyTypes.normal, value: "Normal" },
-  { key: DifficultyTypes.hard, value: "Hard" },
-  { key: DifficultyTypes.very_hard, value: "Very hard" },
-];
+// TODO move to separate constant file
+enum ComplexityTypes {
+  easy = "easy",
+  normal = "normal",
+  hard = "hard",
+}
+
+const complexityLabels: { [key: string]: string } = {
+  [ComplexityTypes.easy]: "Easy",
+  [ComplexityTypes.normal]: "Normal",
+  [ComplexityTypes.hard]: "Hard",
+};
+
+const boardSize = 9;
+
+const boardItems = (): Cell[][] => {
+  const items = new Array(boardSize);
+
+  for (let i = 0; i < boardSize; i++) {
+    items[i] = new Array(boardSize);
+
+    for (let j = 0; j < boardSize; j++) {
+      items[i][j] = {
+        x: j,
+        y: i,
+        id: `${j}${i}`,
+        sectionRowIndex: Math.floor(i / 3),
+        sectionColumnIndex: Math.floor(j / 3),
+        value: "",
+      };
+    }
+  }
+
+  return items;
+};
 
 class Sudoku extends React.Component<SudokuProps, SudokuState> {
-  private boardSize = 9;
-
   private get board(): Cell[][] {
-    const boardItems = new Array(this.boardSize);
+    return boardItems();
+  }
 
-    for (let i = 0; i < this.boardSize; i++) {
-      boardItems[i] = new Array(this.boardSize);
-
-      for (let j = 0; j < this.boardSize; j++) {
-        boardItems[i][j] = { x: i, y: j, id: `${i}${j}`, value: "" };
-      }
-    }
-
-    return boardItems;
+  private get complexities(): Complexity[] {
+    return Object.keys(complexityLabels).map((key) => ({
+      key,
+      label: complexityLabels[key],
+    }));
   }
 
   state: SudokuState = {
-    difficulty: difficulties[2].value,
+    complexity: ComplexityTypes.normal,
     board: this.board,
   };
 
   componentDidMount() {
-    console.log(this.board);
     this.initGame();
   }
 
-  private initGame = (): void => {};
+  private initGame = (): void => {
+    const { complexity } = this.state;
+    // let updatedBoard: Cell[][] = this.board;
+
+    switch (complexity) {
+      case ComplexityTypes.easy: // 7/9
+        this.fillBoard(7);
+        break;
+
+      case ComplexityTypes.normal: // 6/9
+        this.fillBoard(6);
+        break;
+
+      case ComplexityTypes.hard: // 5/9
+        this.fillBoard(5);
+        break;
+
+      default:
+        break;
+    }
+
+    // this.setState({ ...this.state, board: updatedBoard }, () =>
+    //   console.log("BOARD: ", this.state.board)
+    // );
+    // this.setState((state) => ({ ...state, board: updatedBoard }));
+  };
 
   private handleSelectChange = (
     event: React.ChangeEvent<HTMLSelectElement>
   ): void => {
-    this.setState((state) => ({ ...state, difficulty: event.target.value }));
+    const complexity = Object.keys(complexityLabels).find(
+      (key) => complexityLabels[key] === event.target.value
+    ) as ComplexityTypes;
+
+    this.setState({ ...this.state, complexity }, () => this.initGame());
   };
 
-  private handleInputChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-    cell: Cell
-  ): void => {
-    const value = event.target.value.charAt(event.target.value.length - 1);
-    const isPositiveNumber = /^\d*[1-9]\d*$/.test(value);
+  private handleInputChange =
+    (cell: Cell) =>
+    (event: React.ChangeEvent<HTMLInputElement>): void => {
+      const value = event.target.value.charAt(event.target.value.length - 1);
+      const isPositiveNumber = /^\d*[1-9]\d*$/.test(value);
 
-    if (value && !isPositiveNumber) {
-      return;
+      if (value && !isPositiveNumber) {
+        return;
+      }
+
+      const { board } = this.state;
+      const updatedBoard = board.map((rows) =>
+        rows.map((item) => ({
+          ...item,
+          value: cell.id === item.id ? value : item.value,
+        }))
+      );
+
+      this.setState((state) => ({ ...state, board: updatedBoard }));
+    };
+
+  private generateRandomNumber = (
+    min: number = 1,
+    max: number = 9,
+    omitArr: number[] = []
+  ): number => {
+    const randomNumber =
+      Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1)) +
+      Math.ceil(min);
+    const existedRandomNumber = omitArr.some((item) => item === randomNumber);
+
+    if (existedRandomNumber) {
+      return this.generateRandomNumber(min, max, omitArr);
     }
 
+    return randomNumber;
+  };
+
+  private generateRandomNumbers = (
+    quantity: number = 9,
+    max: number = 9,
+    arr: string[] = []
+  ): string[] => {
+    while (arr.length < quantity) {
+      const randomValue = `${Math.floor(Math.random() * max) + 1}`;
+
+      if (!arr.includes(randomValue)) {
+        arr.push(randomValue);
+      }
+    }
+
+    for (let i = 0; i < 9 - quantity; i++) {
+      arr.push("");
+    }
+
+    return arr;
+  };
+
+  private getHorizontalCells = (board: Cell[][], y: number): Cell[] => {
+    return board[y];
+  };
+
+  private getVerticalCells = (board: Cell[][], x: number): Cell[] => {
+    return board.map((rows) => rows.filter((cell) => cell.x === x)[0]);
+  };
+
+  private getSectionCells = (
+    board: Cell[][],
+    sectionRowIndex: number,
+    sectionColumnIndex: number
+  ): Cell[] => {
+    return board
+      .filter(
+        (rows, rowIndex) =>
+          rowIndex >= sectionRowIndex * 3 && rowIndex <= sectionRowIndex * 3 + 2
+      )
+      .map((row) =>
+        row.filter((cell) => cell.sectionColumnIndex === sectionColumnIndex)
+      )
+      .reduce((acc, curr) => [...acc, ...curr], []);
+  };
+
+  private fillBoard = (complexity = 7) => {
     const { board } = this.state;
-    const updatedBoard = board.map((rows) =>
-      rows.map((item) => ({
-        ...item,
-        value:
-          cell.id === item.id ? value.charAt(value.length - 1) : item.value,
-      }))
-    );
+    const updatedBoard = board.map((rows, rowIndex, selfBoard) => {
+      const randomRow = this.generateRandomNumbers(complexity);
+      const randomIndexes: number[] = [];
+
+      return rows.map((cell, cellIndex) => {
+        const randomIndex = this.generateRandomNumber(0, 8, randomIndexes);
+
+        randomIndexes.push(randomIndex);
+
+        return {
+          ...cell,
+          value: randomRow[randomIndex],
+          disabled: !!randomRow[randomIndex],
+        };
+
+        // return rows.map((cell, cellIndex) => {
+        //   const randomIndex = this.generateRandomNumber(0, 8);
+        //   const randomValue = `${randomRow[randomIndex]}`;
+        //   const horizontal = this.getHorizontalCells(selfBoard, cell.y);
+        //   const vertical = this.getVerticalCells(selfBoard, cell.x);
+        //   const section = this.getSectionCells(
+        //     selfBoard,
+        //     cell.sectionRowIndex,
+        //     cell.sectionColumnIndex
+        //   );
+        //   const availableInHorizaontal = horizontal.some(
+        //     (item) => item.value === randomValue
+        //   );
+        //   const availableInVertical = vertical.some(
+        //     (item) => item.value === randomValue
+        //   );
+        //   const availableInSection = section.some(
+        //     (item) => item.value === randomValue
+        //   );
+
+        //   if (
+        //     availableInHorizaontal ||
+        //     availableInVertical ||
+        //     availableInSection
+        //   ) {
+        //     return cell;
+        //   }
+
+        //   return {
+        //     ...cell,
+        //     value: `${randomValue}`,
+        //     disabled: true,
+        //   };
+        // });
+      });
+    });
 
     this.setState((state) => ({ ...state, board: updatedBoard }));
+    console.log("Updated board: ", updatedBoard);
   };
 
   render() {
-    const { difficulty, board } = this.state;
+    const { complexity, board } = this.state;
+    const complexityLabel = complexityLabels[complexity];
 
     return (
       <Container>
-        <Difficulty>
-          <select value={difficulty} onChange={this.handleSelectChange}>
-            {difficulties.map((difficulty) => (
-              <option key={difficulty.key}>{difficulty.value}</option>
+        <ComplexityStyle>
+          <select value={complexityLabel} onChange={this.handleSelectChange}>
+            {this.complexities.map((item) => (
+              <option key={item.key}>{item.label}</option>
             ))}
           </select>
-        </Difficulty>
-        <Board id={`${this.boardSize}`}>
-          {board.map((rows, rowIndex) => (
-            <div key={rows[rowIndex].id} className="sub-board">
-              {rows.map((cell) => (
-                <input
-                  type="text"
-                  className="cell"
-                  key={cell.id}
-                  value={cell.value}
-                  onChange={(event) => this.handleInputChange(event, cell)}
-                />
-              ))}
-            </div>
-          ))}
+        </ComplexityStyle>
+        <Board id={`${boardSize}`}>
+          {board.map((rows) =>
+            rows.map((cell) => (
+              // TODO move to a separate Cell component
+              <input
+                type="text"
+                autoComplete="off"
+                className="cell"
+                style={{
+                  borderRight: cell.x % 3 === 2 ? "2px solid #666" : "none",
+                  borderBottom: cell.y % 3 === 2 ? "2px solid #666" : "none",
+                  color: cell.disabled ? "#666" : "#000",
+                }}
+                id={cell.id}
+                key={cell.id}
+                value={cell.value}
+                disabled={cell.disabled}
+                onChange={this.handleInputChange(cell)}
+              />
+            ))
+          )}
         </Board>
       </Container>
     );
